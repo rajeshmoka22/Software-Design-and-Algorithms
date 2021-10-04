e4bafd0ca94fdc38f779c6c8f87e2767c2a9fe12
Rajesh Moka <rajeshmoka@Rajeshs-MacBook.local>
2021-10-04 20:14:00 +0530

    added types as per review comments

--- Basics of OOP2/Page.ts
+++ Basics of OOP2/Page.ts
@@ -1,12 +1,16 @@
 type PageMaterial = 'glossy paper' | 'simple paper' | 'paper'; // added 'paper' to match page.spec.ts
 type PageType = 'with images' | 'with article' | 'with some content' | 'with text'; // added 'with some content' to match page.spec.ts
 
+
+type PageMaterial = 'glossy paper' | 'simple paper' | 'paper'; // added 'paper' to match page.spec.ts
+type PageType = 'with images' | 'with article' | 'with some content' | 'with text'; // added 'with some content' to match page.spec.ts
+
 class Page {
   pageNumber: number;
-  pageType: string;
-  pageMaterial: string;
+  pageType: PageType;
+  pageMaterial: PageMaterial;
 
-  constructor(pageNumber: number, pageType: string, pageMaterial: string) {
+  constructor(pageNumber: number, pageType: PageType, pageMaterial: PageMaterial) {
     this.pageNumber = pageNumber;
     this.pageType = pageType;
     this.pageMaterial = pageMaterial;
